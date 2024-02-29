f6aa695b752c549f0f81ed847cd72581c62768b6
ufrj_sousa <ufrj_sousa@hcpa.edu.br>
2024-02-29 18:29:50 -0300

    Conexão api google sheets

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('hello world!')
-print('olá mundo!')
+from model import Vagas
+from util import google_sheets_util as gs
+import pandas as pd
+
+lista_vagas = []
+
+#Criação de objetos vaga
+vaga1 = Vagas.Vagas("Estágio de Engenharia Química", "Esta é a descrição da vaga", "UFRJ", "http://ufrj.br")
+
+lista_vagas.append(vaga1)
+
+vaga2 = Vagas.Vagas("Estágio de Química Industrial", "Esta é a descrição da vaga2", "UFRJ", "http://ufrj.br")
+
+lista_vagas.append(vaga2)
+
+data = pd.DataFrame.from_records([v.to_dict() for v in lista_vagas])
+
+gs.writeToSheets(data)
--- model/Vagas.py
+++ model/Vagas.py
@@ -1,14 +1,17 @@
 class Vagas:
-    def __init__(self, titulo, descricao, requisitos, nome_empresa, bolsa_auxilio, modalidade, localizacao,  origem, link):
+    def __init__(self, titulo, descricao, empresa, link):
         self.titulo = titulo
         self.descricao = descricao
-        self.requisitos = requisitos
-        self.nome_empresa = nome_empresa
-        self.bolsa_auxilio = bolsa_auxilio
-        self.modalidade = modalidade
-        self.localizacao = localizacao
-        self.origem = origem
+        self.empresa = empresa
         self.link = link
 
     def __str__(self):
-        return f"Vaga de Estágio:\nTítulo: {self.titulo}\nDescrição: {self.descricao}\nCompetências: {', '.join(self.requisitos)}\nNome da Empresa: {self.nome_empresa}\nBolsa Auxílio: R${self.bolsa_auxilio}\nModalidade: {self.modalidade}\nOutros Campos: {self.outros_campos}"
+        return f"Vaga de Estágio:\nTítulo: {self.titulo}\nDescrição: {self.descricao}\nNome da empresa: {self.empresa}\nLink: {self.link}"
+
+    def to_dict(self):
+        return{
+            'Título': self.titulo,
+            'Descricao': self.descricao,
+            'Empresa': self.empresa,
+            'Link': self.link
+        }
