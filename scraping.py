11453168fc7b184f467c0ecfcf0fb7239e5dcce6
paiva-j <131556110+paiva-j@users.noreply.github.com>
2024-07-25 18:47:33 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,10 +1,10 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 from model import Vagas
 
 options = Options()
